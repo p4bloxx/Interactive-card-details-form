182e49949f9de389abae86870f47d76e0eba6c66
Pablo Dettori <106544349+p4bloxx@users.noreply.github.com>
2023-10-20 16:04:41 +0200

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,138 @@
+
+const input = document.querySelectorAll('input');
+let inputArr = Array.from(input);
+const btn = document.querySelector('button');
+const form = document.querySelector('form')
+const confirmBox = document.querySelector('.confirm-box')
+const numb = document.querySelector('.card-numb')
+const holder = document.querySelector('.card-name')
+const date = document.querySelector('.card-date')
+const cvc = document.querySelector('.card-cvc')
+const err = document.querySelectorAll('.err')
+const errArray = Array.from(err)
+const dateArray = [document.querySelector('#date'), document.querySelector('#year')];
+const dateBox = document.querySelector('.month-year');
+
+
+let checkDate = false;
+let checkName = false;
+let checkCvc = false
+let checkNumber = false;
+
+  inputArr.forEach((inp) => {
+  inp.addEventListener('input', (e) => {
+    const parentInput = inp.parentElement;
+    
+    //check date (MM YY)
+    if(inputArr[2].value === '' || inputArr[3].value === ''){
+      if(inputArr[2].value === ''){
+        inputArr[2].style.borderColor = 'red'
+        dateBox.lastElementChild.innerHTML = "month not correct";
+
+      } else {
+        inputArr[2].style.borderColor = ''
+      }
+      
+      if(inputArr[3].value === ''){
+        inputArr[3].style.borderColor = 'red'
+       } else {
+        inputArr[3].style.borderColor = ''
+       }
+
+      dateBox.lastElementChild.innerHTML = "can't be blank"
+      date.innerHTML = `00/00`
+      checkDate = false;
+    } else if (!inputArr[2].value.match(/^\d+$/) || !inputArr[3].value.match(/^\d+$/)){
+      dateBox.lastElementChild.innerHTML = "Wrong format, numbers only"
+      inp.style.borderColor = 'red'
+      checkDate = false;
+    } else {
+      dateBox.lastElementChild.innerHTML = ""
+      inp.style.borderColor = ''
+      date.innerHTML = `${dateArray[0].value}/${dateArray[1].value}`
+      checkDate = true;
+    }
+
+    if(inputArr[2].value > 12) {
+      console.log(inputArr[2].value)
+      inputArr[2].style.borderColor = 'red'
+      dateBox.lastElementChild.innerHTML = "month not correct"
+      checkDate = false;
+    } 
+    
+    
+    //check nameHodler
+    if (inputArr[0].value === '') {
+      inputArr[0].style.borderColor = 'red'
+      inputArr[0].nextElementSibling.innerHTML = "can't be blank";
+      holder.innerHTML = `JANE APPLESEED`
+      checkName = false;
+
+    } else if(!inputArr[0].value.match(/^[a-zA-Z\s]+$/) && inputArr[0].value !== '') {
+      inputArr[0].nextElementSibling.innerHTML = 'Wrong format, letters only'
+      inputArr[0].style.borderColor = 'red'
+      checkName = false;
+
+    } else {
+      inputArr[0].nextElementSibling.innerHTML = ''
+      inputArr[0].style.borderColor = ''
+      holder.innerHTML = `${inputArr[0].value.toUpperCase()}`
+      checkName = true;
+    }
+    
+
+    //check cvc
+    if(inputArr[4].value === '') {
+      inputArr[4].style.borderColor = 'red'
+      inputArr[4].nextElementSibling.innerHTML = "can't be blank"
+      cvc.innerHTML = `000`
+      checkCvc = false
+
+    } else if (!inputArr[4].value.match(/^\d+$/)){
+      inputArr[4].nextElementSibling.innerHTML = "Wrong format, numbers only"
+      inputArr[4].style.borderColor = 'red'
+      checkCvc = false
+
+    } else {
+      inputArr[4].style.borderColor = ''
+      inputArr[4].nextElementSibling.innerHTML = ""
+      cvc.innerHTML = `${inputArr[4].value}`
+      checkCvc = true;
+    }
+
+    //check numberCard
+    if(inputArr[1].value === ''){
+      inputArr[1].style.borderColor = 'red'
+      inputArr[1].nextElementSibling.innerHTML = "can't be blank"
+      numb.innerHTML = '0000 0000 0000 0000';
+      checkNumber = false
+
+    } else if (!inputArr[1].value.match(/^[0-9\s]*$/)){
+      inputArr[1].style.borderColor = 'red'
+      inputArr[1].nextElementSibling.innerHTML = "Wrong format, numbers only"
+      checkNumber = false
+
+    } else{
+      inputArr[1].nextElementSibling.innerHTML = ""
+      inputArr[1].style.borderColor = ''
+      let inputNumb = inputArr[1].value.toString();
+      let check = inputNumb.match(/\d{4}/g);
+      let checkString = check.join(" ")
+      numb.innerHTML = checkString;
+      checkNumber = true
+    }
+  })
+}) 
+
+btn.addEventListener('click', () => {
+  if(checkName && checkDate && checkNumber && checkCvc){
+    form.style.display = 'none'
+    confirmBox.style.display = 'flex'
+    setTimeout(confirm, 3000)
+  }
+
+  function confirm () {
+    confirmBox.style.display = 'none'
+    form.style.display = 'flex'
+  }
+})
---
+++ README.md
@@ -0,0 +1,76 @@
+# Frontend Mentor - Interactive card details form solution
+
+This is a solution to the [Interactive card details form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Fill in the form and see the card details update in real-time
+- Receive error messages when the form is submitted if:
+  - Any input field is empty
+  - The card number, expiry date, or CVC fields are in the wrong format
+- View the optimal layout depending on their device's screen size
+- See hover, active, and focus states for interactive elements on the page
+
+### Screenshot
+
+![](SCREENSHOTS/mobile%20view.png)
+![](SCREENSHOTS/tablet%20view.png)
+![](SCREENSHOTS/desktop%20view.png)
+![](SCREENSHOTS/desktop%20view%20-%20error%20state.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Sass (Preprocessor) for CSS [https://sass-lang.com/]
+- SCSS syntax
+- SASS variables
+- Javascript
+
+### What I learned
+
+Hi everyone, in this challenge I had some difficulties regarding the management of form errors, in particular regarding the control of the text inserted in the various inputs, and that it was in line with the characters allowed for each type of input.
+
+Also regarding the **card number** field as I had to understand how to automatically divide the numbers into blocks of 4, but thanks to the help of some online resources, I was able to understand how it might work; it was a good workout ;)
+
+### Continued development
+
+I feel like I have to continue a lot with **Javascript** and understand well the various logics to different commonly used problems regarding front-end development, and to do that I have to practice, nothing else ;)
+
+### Useful resources
+
+I found some great resources on some https://stackoverflow.com/ threads that helped me understand the problem better.
+
+## Author
+
+- Frontend Mentor - [@p4bloxx](https://www.frontendmentor.io/profile/p4bloxx)
+- Github - [p4abloxx](https://github.com/p4bloxx)
+
+## Acknowledgments
